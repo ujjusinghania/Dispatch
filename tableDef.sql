e8b3c3ea071463c7ea4237c5717ff7086fec6d3b
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-11-01 14:50:30 -0400

    Updated Table Definitions
    - Wrote table def for person.
    - Wrote table def for friendgroup.
    - Wrote table def for member.

---
+++ tableDef.sql
@@ -0,0 +1,25 @@
+CREATE TABLE person (
+    username VARCHAR(12),
+    password VARCHAR(14),
+    firstname VARCHAR(20),
+    lastname VARCHAR(20),
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE friendgroup (
+    name VARCHAR(40),
+    username VARCHAR(12),
+    description VARCHAR(100),
+    PRIMARY KEY (username, name),
+    FOREIGN KEY (username) REFERENCES person(username)
+);
+
+CREATE TABLE member (
+    username VARCHAR(12),
+    name VARCHAR(40),
+    PRIMARY KEY (username, name)
+    FOREIGN KEY (username) REFERENCES person(username),
+    FOREIGN KEY (name) REFERENCES friendgroup(name)
+);
+
+
