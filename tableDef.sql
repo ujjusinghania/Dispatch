61a6bfcd8ad30365257ee78b042786974a24ac9c
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-11-02 11:06:33 -0400

    Completed TableDef.sql for Dispatch

--- tableDef.sql
+++ tableDef.sql
@@ -32,31 +32,33 @@ CREATE TABLE comment (
     FOREIGN KEY  (contentID)  REFERENCES content(contentID) ON DELETE CASCADE,
 );
 
-CREATE TABLE content(
+CREATE TABLE content (
     contentID       INTEGER,
     contentDate     DATE,
     file_path       VARCHAR(50),
     contentName     VARCHAR(50),
     is_pub          BOOLEAN,
     username        VARCHAR(12),
-<<<<<<< HEAD
     PRIMARY KEY (contentID),
     FOREIGN KEY (username) REFERENCES person(username) ON DELETE SET NULL 
 );
-=======
-    PRIMARY KEY (contentID)
-    FOREIGN KEY (username) REFERENCES person(username)
+
+CREATE TABLE share (
+    contentID   INTEGER,
+    name        VARCHAR(40),
+    PRIMARY KEY (name, contentID),
+    FOREIGN KEY (name) REFERENCES friendgroup(name) ON DELETE CASCADE,
+    FOREIGN KEY (contentID) REFERENCES content(contentID) ON DELETE CASCADE
 );
 
-CREATE TABLE tag(
+CREATE TABLE tag (
     status          Boolean,
     contentID       INTEGER,
-    taggedusername        VARCHAR(12),
-    taggerusername        VARCHAR(12),
-    time_stamp   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    taggedusername  VARCHAR(12),
+    taggerusername  VARCHAR(12),
+    time_stamp      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (taggedusername, taggerusername, contentID)
     FOREIGN KEY (taggedusername) REFERENCES person(username)
     FOREIGN KEY (taggerusername) REFERENCES person(username)
     FOREIGN KEY (contentID) REFERENCES content(contentID)
 );
->>>>>>> 24b9a32a69ee1a50b7bd8cc248075fd64ab17f43
