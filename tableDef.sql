eecfad63a24620eb520266232b85cad74d0bcbb7
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-11-03 15:18:56 -0400

    TableDef works on MAMP and WAMP 😭

--- tableDef.sql
+++ tableDef.sql
@@ -66,7 +66,7 @@ CREATE TABLE tag (
     taggerusername  VARCHAR(12),    # Username of the person tagging another
     time_stamp      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, # Timestamp of the tag
     PRIMARY KEY (taggedusername, taggerusername, contentID),
-    FOREIGN KEY (taggedusername) REFERENCES person(username) ON DELETE SET NULL,
-    FOREIGN KEY (taggerusername) REFERENCES person(username) ON DELETE SET NULL,
+    FOREIGN KEY (taggedusername) REFERENCES person(username) ON DELETE CASCADE,
+    FOREIGN KEY (taggerusername) REFERENCES person(username) ON DELETE CASCADE,
     FOREIGN KEY (contentID) REFERENCES content(contentID) ON DELETE CASCADE
 );
