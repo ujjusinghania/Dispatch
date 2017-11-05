63c1e5ee06ea2eac13e024305fb935dc1a0af290
JackLangerman <JackMLangerman@gmail.com>
2017-11-05 12:50:40 -0500

    Update form_reset.sh

--- form_reset.sh
+++ form_reset.sh
@@ -1,6 +1,6 @@
-printf "# DROP ALL TABLES\n" 		  >  	reset.sql
-cat dropAll.sql     	   			    >> 	reset.sql
-printf "\n\n# CREATE TABLES\n" 		>>	reset.sql	
-cat tableDef.sql    				      >> 	reset.sql
+printf "# DROP ALL TABLES\n"			>		reset.sql
+cat dropAll.sql										>>	reset.sql
+printf "\n\n# CREATE TABLES\n"		>>	reset.sql	
+cat tableDef.sql									>>	reset.sql
 printf "\n\n# POPULATE TABLES\n"	>>	reset.sql
-cat tableInsert.sql 				      >> 	reset.sql
+cat tableInsert.sql								>>	reset.sql
