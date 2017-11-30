809a1a476ab2c3450acbac3dc78f13d297b99980
Abir Hassan <ash521@nyu.edu>
2017-11-30 14:29:47 -0500

    Typo

--- tableDef.sql
+++ tableDef.sql
@@ -4,7 +4,7 @@ CREATE TABLE Person
     password VARCHAR (50),
     first_name VARCHAR (50),
     last_name VARCHAR (50),
-	color VARCHAR (6),
+	color VARCHAR (7),
     PRIMARY KEY (username)
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
