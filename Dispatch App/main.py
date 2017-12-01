1006d8efd90fd9268176533893d1293a97945c9e
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-11-30 21:54:24 -0500

    Used Flask Blueprints to divide file into two

--- Dispatch App/main.py
+++ Dispatch App/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template, session, url_for, redirect
 from flask import request
+from flask import Blueprint
 import pymysql.cursors
 import hashlib
+from friends import friends_blueprint
 
 app = Flask(__name__)
+app.register_blueprint(friends_blueprint)
 
 # this is for pulling the port and database password from environment variables
 import os
@@ -331,38 +334,6 @@ def addMessage():
                  '?groupSelected=' + session['groupSelected'][0] +
                  '&username_creator=' + session['groupSelected'][1]
                  )
-
-# Functions pertaining to addition/deletion/viewing of Friends on the App. 
-
-@app.route('/home/friendhome')
-def viewFriendHome():
-	return render_template('friendhome.html')
-
-@app.route('/home/friendhome/addfriend')
-def addFriend():
-	return render_template('addfriend.html')
-
-@app.route('/home/friendhome/viewfriends')
-def viewFriends():
-	username = session['username']
-	cursor = conn.cursor()
-
-	# Finding friends who you sent a friend request to. 
-	query = 'SELECT first_name, last_name FROM friends JOIN person ON friends.friend_send_username = person.username WHERE accepted_request = TRUE AND friend_receive_username = %s'
-	cursor.execute(query, (username))
-	requestSendFriends = cursor.fetchall()
-
-	# Finding friends who you received a friend request from. 
-	query = 'SELECT first_name, last_name FROM friends JOIN person ON friends.friend_receive_username = person.username WHERE accepted_request = TRUE AND friend_send_username = %s'
-	cursor.execute(query, (username))
-	requestReceiveFriends = cursor.fetchall()
-
-	return render_template('viewfriends.html', friends=requestReceiveFriends+requestSendFriends)
-
-@app.route('/home/friendhome/friendrequest')
-def viewFriendRequests():
-	return render_template('friendrequest.html')
-
 
 def md5(password):
 	# encode and hash password
