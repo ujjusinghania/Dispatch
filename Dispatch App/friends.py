72a03558166fa533b1ce162efb06a442c103cf4e
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-12-06 15:50:58 -0500

    Recalling friend requests works now!

--- Dispatch App/friends.py
+++ Dispatch App/friends.py
@@ -38,7 +38,7 @@ def addFriendAuth():
 	# Finding all people that are like the user's entered string and that are not in
 	# the friends list already (pending request or already friends)
 
-	query = 'SELECT first_name, last_name, username FROM PERSON WHERE first_name LIKE %s AND last_name LIKE %s AND username != %s AND username NOT IN( SELECT DISTINCT friend_send_username FROM friends WHERE friend_receive_username = %s ) AND username NOT IN( SELECT DISTINCT friend_receive_username FROM friends WHERE friend_send_username = %s )'
+	query = 'SELECT first_name, last_name, username FROM PERSON WHERE first_name LIKE %s AND last_name LIKE %s AND username != %s AND username NOT IN (SELECT DISTINCT friend_send_username FROM friends WHERE friend_receive_username = %s) AND username NOT IN( SELECT DISTINCT friend_receive_username FROM friends WHERE friend_send_username = %s )'
 
 	cursor.execute(query, (searchFirstName+"%", searchLastName+"%", username, username, username))
 	people = cursor.fetchall()
@@ -118,3 +118,27 @@ def sendFriendRequest():
 	conn.commit()
 	cursor.close()
 	return redirect(url_for('.addFriend'))
+
+@friends_blueprint.route('/home/friendhome/sentfriendrequest')
+def viewSentFriendRequests(): 
+	username = session['username']
+	cursor = conn.cursor()
+
+	# Finding friends who you sent a friend request to. 
+	query = 'SELECT first_name, last_name, username FROM friends JOIN person ON friends.friend_receive_username = person.username WHERE accepted_request = FALSE AND friend_send_username = %s'
+	cursor.execute(query, (username))
+	frequests = cursor.fetchall()
+	cursor.close()
+	return render_template('sentfriendrequest.html', friendRequests=frequests)
+
+@friends_blueprint.route('/home/friendhome/sentfriendrequest/recallRequest')
+def recallRequest():
+	sender = session['username']
+	cursor = conn.cursor()
+	receiver = request.args.get('sentTo')
+	query = 'DELETE FROM friends WHERE friend_send_username = %s AND friend_receive_username = %s AND accepted_request = FALSE'
+	cursor.execute(query, (sender, receiver))
+	conn.commit()
+	cursor.close()
+
+	return redirect(url_for('.viewSentFriendRequests'))
