b6f033967483e12cb1ee0f72e1a94bd26d2926eb
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-12-08 23:52:08 -0500

    Added some provisions for delete account feature

--- tableDef.sql
+++ tableDef.sql
@@ -17,7 +17,7 @@ CREATE TABLE Content(
     public BOOLEAN,             #
     caption TEXT,               # textual description of content
     PRIMARY KEY (id),
-    FOREIGN KEY (username) REFERENCES Person (username)
+    FOREIGN KEY (username) REFERENCES Person (username) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE TextContent (
@@ -48,7 +48,7 @@ CREATE TABLE FriendGroup
     username VARCHAR (50),
     description VARCHAR (50),
     PRIMARY KEY (group_name, username),
-    FOREIGN KEY (username) REFERENCES Person(username)
+    FOREIGN KEY (username) REFERENCES Person(username) ON DELETE CASCADE
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 ALTER TABLE friendgroup ADD INDEX(username);
@@ -62,7 +62,7 @@ CREATE TABLE Comment
     comment_text VARCHAR (250),
     PRIMARY KEY (id, username, timest),
     FOREIGN KEY (id) REFERENCES Content(id),
-    FOREIGN KEY (username) REFERENCES Person(username)
+    FOREIGN KEY (username) REFERENCES Person(username) ON DELETE CASCADE
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -73,7 +73,7 @@ CREATE TABLE Share (
     username VARCHAR (50),
     PRIMARY KEY (id, group_name, username),
     FOREIGN KEY (id) REFERENCES Content(id),
-    FOREIGN KEY (group_name, username) REFERENCES FriendGroup(group_name, username)
+    FOREIGN KEY (group_name, username) REFERENCES FriendGroup(group_name, username) ON DELETE CASCADE
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -83,8 +83,8 @@ CREATE TABLE Member (
     group_name VARCHAR (50),
     username_creator VARCHAR (50),
     PRIMARY KEY (username, group_name, username_creator),
-    FOREIGN KEY (username) REFERENCES Person(username),
-    FOREIGN KEY (group_name, username_creator) REFERENCES FriendGroup(group_name, username)
+    FOREIGN KEY (username) REFERENCES Person(username) ON DELETE CASCADE,
+    FOREIGN KEY (group_name, username_creator) REFERENCES FriendGroup(group_name, username) 
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -96,8 +96,8 @@ CREATE TABLE Tag (
     status BOOLEAN,
     PRIMARY KEY (id, username_tagger, username_taggee),
     FOREIGN KEY (id) REFERENCES Content(id),
-    FOREIGN KEY (username_tagger) REFERENCES Person(username),
-    FOREIGN KEY (username_taggee) REFERENCES Person(username)
+    FOREIGN KEY (username_tagger) REFERENCES Person(username) ON DELETE CASCADE, 
+    FOREIGN KEY (username_taggee) REFERENCES Person(username) ON DELETE CASCADE
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -105,8 +105,8 @@ CREATE TABLE Friends (
     friend_send_username VARCHAR (50),
     friend_receive_username VARCHAR (50),
     accepted_request BOOLEAN,
-    FOREIGN KEY (friend_receive_username) REFERENCES Person(username),
-    FOREIGN KEY (friend_send_username) REFERENCES Person(username)
+    FOREIGN KEY (friend_receive_username) REFERENCES Person(username) ON DELETE CASCADE,
+    FOREIGN KEY (friend_send_username) REFERENCES Person(username) ON DELETE CASCADE
 )
 ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
